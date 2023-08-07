7ce795f6967025b84a947077aac83fd8b7e762a4
Emir Büçkün <emirbuckun@hotmail.com>
2023-08-07 16:20:30 +0300

    Database updated

---
+++ RealEstateApp.Api/Migrations/RealEstate/20230807131920_AttrUpdate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateApp.Api.Migrations.RealEstate
+{
+    /// <inheritdoc />
+    public partial class AttrUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
