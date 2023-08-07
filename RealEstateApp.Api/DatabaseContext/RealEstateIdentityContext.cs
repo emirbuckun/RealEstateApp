35346d057d9338185af7bf0aa6f55ce3559fe3f7
Emir Büçkün <emirbuckun@hotmail.com>
2023-08-07 14:03:29 +0300

    Identity db context created

---
+++ RealEstateApp.Api/DatabaseContext/RealEstateIdentityContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RealEstateApp.Api.DatabaseContext
+{
+  public class RealEstateIdentityContext : IdentityDbContext<IdentityUser>
+  {
+    public RealEstateIdentityContext(DbContextOptions<RealEstateIdentityContext> options)
+    : base(options) { }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      base.OnModelCreating(builder);
+    }
+  }
+}
