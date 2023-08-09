71ca281407f8a851a36518dec4a7fd33c9932394
Emir Büçkün <emirbuckun@hotmail.com>
2023-08-09 17:17:21 +0300

    Type and status id added to estate info DTO

--- RealEstateApp.Api/DTO/Estate/InfoEstate.cs
+++ RealEstateApp.Api/DTO/Estate/InfoEstate.cs
@@ -10,7 +10,9 @@ namespace RealEstateApp.Api.DTO.Estate
     public double Longitude { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
+    public int EstateTypeId { get; set; }
     public string EstateType { get; set; }
+    public int EstateStatusId { get; set; }
     public string EstateStatus { get; set; }
 
     public InfoEstate(ET.Estate estate)
@@ -21,7 +23,9 @@ namespace RealEstateApp.Api.DTO.Estate
       Longitude = estate.Longitude;
       StartDate = estate.StartDate;
       EndDate = estate.EndDate;
+      EstateTypeId = estate.EstateType != null ? estate.EstateType.Id : 0;
       EstateType = estate.EstateType != null ? estate.EstateType.Name ?? string.Empty : string.Empty;
+      EstateStatusId = estate.EstateStatus != null ? estate.EstateStatus.Id : 0;
       EstateStatus = estate.EstateStatus != null ? estate.EstateStatus.Name ?? string.Empty : string.Empty;
     }
   }
