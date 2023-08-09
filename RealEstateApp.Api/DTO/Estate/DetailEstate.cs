d61e79368453f57a3fdcfe1224cbcea41594e996
Emir Büçkün <emirbuckun@hotmail.com>
2023-08-09 22:20:15 +0300

    Detail estate DTO created

---
+++ RealEstateApp.Api/DTO/Estate/DetailEstate.cs
@@ -0,0 +1,33 @@
+using ET = RealEstateApp.Api.Entity;
+using BDTO = RealEstateApp.Api.DTO.BaseDTO;
+using RealEstateApp.Api.DTO.Price;
+
+namespace RealEstateApp.Api.DTO.Estate
+{
+  public class DetailEstate : BDTO.BaseDTO
+  {
+    public string Name { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int EstateTypeId { get; set; }
+    public int EstateStatusId { get; set; }
+    public ICollection<InfoPrice> Prices { get; set; }
+
+    public DetailEstate(ET.Estate estate)
+    {
+      Id = estate.Id;
+      Name = estate.Name ?? string.Empty;
+      Latitude = estate.Latitude;
+      Longitude = estate.Longitude;
+      StartDate = estate.StartDate;
+      EndDate = estate.EndDate;
+      EstateTypeId = estate.EstateType != null ? estate.EstateType.Id : 0;
+      EstateStatusId = estate.EstateStatus != null ? estate.EstateStatus.Id : 0;
+      var priceList = new List<InfoPrice>();
+      estate.Prices?.ToList().ForEach(x => priceList.Add(new InfoPrice(x)));
+      Prices = priceList;
+    }
+  }
+}
