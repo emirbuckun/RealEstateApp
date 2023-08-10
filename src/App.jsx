68db6b5519e91f666ad35bba7573413fa0f4974b
Emir Büçkün <emirbuckun@hotmail.com>
2023-08-10 17:22:43 +0300

    Language support added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { Suspense } from "react";
 import AppRouter from "./AppRouter";
 import Navbar from "./pages/Navbar";
 
 function App() {
   return (
-    <>
+    <Suspense
+      fallback={
+        <h3 className="d-flex justify-content-center align-items-center">
+          🌀 Loading...
+        </h3>
+      }
+    >
       <Navbar />
       <AppRouter />
-    </>
+    </Suspense>
   );
 }
 
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -2,9 +2,13 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import NavbarB from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
 import { validateToken } from "/src/services/AuthService";
+import { LANGUAGES } from "/src/constants";
+import { useTranslation } from "react-i18next";
 
 const Navbar = () => {
+  const { i18n, t } = useTranslation();
   const list = [
     { name: "Currencies", url: "/currencies" },
     { name: "Estates", url: "/estates" },
@@ -14,6 +18,12 @@ const Navbar = () => {
     { name: "Prices", url: "/prices" },
     { name: "Map", url: "/map" },
   ];
+
+  const onChangeLang = (e) => {
+    const lang_code = e.target.value;
+    i18n.changeLanguage(lang_code);
+  };
+
   return (
     <NavbarB fixed="top" collapseOnSelect expand="md">
       <Container>
@@ -21,7 +31,7 @@ const Navbar = () => {
         <NavbarB.Toggle aria-controls="responsive-navbar-nav" />
         <NavbarB.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/">{t("home")}</Nav.Link>
             <NavDropdown title="Pages" id="collasible-nav-dropdown">
               {list.map((item, index) => {
                 const { name, url } = item;
@@ -33,6 +43,15 @@ const Navbar = () => {
               })}
             </NavDropdown>
           </Nav>
+          <Form>
+            <Form.Select onChange={onChangeLang}>
+              {LANGUAGES.map(({ code, label }) => (
+                <option key={code} value={code}>
+                  {label}
+                </option>
+              ))}
+            </Form.Select>
+          </Form>
           <Nav>
             {!validateToken() ? (
               <Nav.Link href="/auth">Login/Register</Nav.Link>
