97f9234a1944320449befaf2d2fae711994392ba
Emir Büçkün <emirbuckun@hotmail.com>
2023-08-15 14:27:05 +0300

    Chart component created and used in dashboard

---
+++ src/components/Chart.jsx
@@ -0,0 +1,44 @@
+import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
+
+export const Chart = ({ data }) => {
+  const COLORS = ["#3366ff", "#ff0066", "#99ff33", "#FF8042", "#AF19FF"];
+
+  const CustomTooltip = ({ active, payload }) => {
+    if (active) {
+      return (
+        <div
+          className="custom-tooltip"
+          style={{
+            backgroundColor: "#123",
+            padding: "5px",
+            border: "1px solid #cccc",
+          }}
+        >
+          <label>{`${payload[0].name}: ${payload[0].value}`}</label>
+        </div>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <PieChart width={730} height={300}>
+      <Pie
+        data={data}
+        color="#000000"
+        dataKey="value"
+        nameKey="name"
+        cx="50%"
+        cy="50%"
+        outerRadius={120}
+        fill="#8884d8"
+      >
+        {data.map((_, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+        ))}
+      </Pie>
+      <Tooltip content={<CustomTooltip />} />
+      <Legend />
+    </PieChart>
+  );
+};
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
-import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
 import { getEstates } from "/src/services/EstateService";
 import { useTranslation } from "react-i18next";
-import { Col, Row } from "react-bootstrap/";
+import { Col, Row } from "react-bootstrap";
+import { Chart } from "/src/components/Chart";
 
 const Dashboard = () => {
   const { t } = useTranslation();
@@ -16,7 +16,6 @@ const Dashboard = () => {
     setState({ error: null, loading: true });
     getEstates()
       .then((response) => {
-        console.log(response);
         if (response.status == 200) {
           setState({ estates: response.data, loading: false });
         } else
@@ -54,26 +53,6 @@ const Dashboard = () => {
     fetchEstates();
   }, []);
 
-  const COLORS = ["#3366ff", "#ff0066", "#99ff33", "#FF8042", "#AF19FF"];
-
-  const CustomTooltip = ({ active, payload }) => {
-    if (active) {
-      return (
-        <div
-          className="custom-tooltip"
-          style={{
-            backgroundColor: "#123",
-            padding: "5px",
-            border: "1px solid #cccc",
-          }}
-        >
-          <label>{`${payload[0].name}: ${payload[0].value}`}</label>
-        </div>
-      );
-    }
-    return null;
-  };
-
   return (
     <>
       <h3>{t("dashboard")}</h3>
@@ -86,51 +65,11 @@ const Dashboard = () => {
             <Row>
               <Col sm={12} md={8} lg={6}>
                 <h5>{t("types")}</h5>
-                <PieChart width={730} height={300}>
-                  <Pie
-                    data={pieTypeData}
-                    color="#000000"
-                    dataKey="value"
-                    nameKey="name"
-                    cx="50%"
-                    cy="50%"
-                    outerRadius={120}
-                    fill="#8884d8"
-                  >
-                    {pieTypeData.map((entry, index) => (
-                      <Cell
-                        key={`cell-${index}`}
-                        fill={COLORS[index % COLORS.length]}
-                      />
-                    ))}
-                  </Pie>
-                  <Tooltip content={<CustomTooltip />} />
-                  <Legend />
-                </PieChart>
+                <Chart data={pieTypeData} />
               </Col>
               <Col sm={12} md={8} lg={6}>
                 <h5>{t("statuses")}</h5>
-                <PieChart width={730} height={300}>
-                  <Pie
-                    data={pieStatusData}
-                    color="#000000"
-                    dataKey="value"
-                    nameKey="name"
-                    cx="50%"
-                    cy="50%"
-                    outerRadius={120}
-                    fill="#8884d8"
-                  >
-                    {pieStatusData.map((entry, index) => (
-                      <Cell
-                        key={`cell-${index}`}
-                        fill={COLORS.reverse()[index % COLORS.length]}
-                      />
-                    ))}
-                  </Pie>
-                  <Tooltip content={<CustomTooltip />} />
-                  <Legend />
-                </PieChart>
+                <Chart data={pieStatusData} />
               </Col>
             </Row>
           </>
