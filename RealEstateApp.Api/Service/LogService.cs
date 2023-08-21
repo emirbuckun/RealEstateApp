89d5ea4d9eba297e414524a4f73865f842831118
Emir Büçkün <emirbuckun@hotmail.com>
2023-08-21 18:16:29 +0300

    New method added to get last records

--- RealEstateApp.Api/Service/LogService.cs
+++ RealEstateApp.Api/Service/LogService.cs
@@ -4,36 +4,41 @@ using MongoDB.Driver;
 
 namespace RealEstateApp.Api.Service
 {
-  public class LogService
-  {
-    private readonly IMongoCollection<Log> _logsCollection;
+	public class LogService
+	{
+		private readonly IMongoCollection<Log> _logsCollection;
 
-    public LogService(
-        IOptions<LogDatabaseSettings> logDatabaseSettings)
-    {
-      var mongoClient = new MongoClient(
-          logDatabaseSettings.Value.ConnectionString);
+		public LogService(
+				IOptions<LogDatabaseSettings> logDatabaseSettings)
+		{
+			var mongoClient = new MongoClient(
+					logDatabaseSettings.Value.ConnectionString);
 
-      var mongoDatabase = mongoClient.GetDatabase(
-          logDatabaseSettings.Value.DatabaseName);
+			var mongoDatabase = mongoClient.GetDatabase(
+					logDatabaseSettings.Value.DatabaseName);
 
-      _logsCollection = mongoDatabase.GetCollection<Log>(
-          logDatabaseSettings.Value.CollectionName);
-    }
+			_logsCollection = mongoDatabase.GetCollection<Log>(
+					logDatabaseSettings.Value.CollectionName);
+		}
 
-    public async Task<List<Log>> GetAsync() =>
-        await _logsCollection.Find(_ => true).ToListAsync();
+		public async Task<List<Log>> GetAsync() =>
+				await _logsCollection.Find(_ => true).ToListAsync();
 
-    public async Task<Log?> GetAsync(string id) =>
-        await _logsCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+		public List<Log> GetLastRecords() =>
+				_logsCollection.AsQueryable()
+				.OrderByDescending(x => x.UtcTimeStamp)
+				.Take(100).ToList();
 
-    public async Task CreateAsync(Log newLog) =>
-        await _logsCollection.InsertOneAsync(newLog);
+		public async Task<Log?> GetAsync(string id) =>
+				await _logsCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
 
-    public async Task UpdateAsync(string id, Log updatedLog) =>
-        await _logsCollection.ReplaceOneAsync(x => x.Id == id, updatedLog);
+		public async Task CreateAsync(Log newLog) =>
+				await _logsCollection.InsertOneAsync(newLog);
 
-    public async Task RemoveAsync(string id) =>
-        await _logsCollection.DeleteOneAsync(x => x.Id == id);
-  }
+		public async Task UpdateAsync(string id, Log updatedLog) =>
+				await _logsCollection.ReplaceOneAsync(x => x.Id == id, updatedLog);
+
+		public async Task RemoveAsync(string id) =>
+				await _logsCollection.DeleteOneAsync(x => x.Id == id);
+	}
 }
