173738533b37e100ea7a571508388fead5dc4781
Emir Büçkün <emirbuckun@hotmail.com>
2023-08-21 18:17:09 +0300

    Web socket connection added to log controller get method

--- RealEstateApp.Api/Controller/LogController.cs
+++ RealEstateApp.Api/Controller/LogController.cs
@@ -1,3 +1,6 @@
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using RealEstateApp.Api.Model;
 using RealEstateApp.Api.Service;
@@ -12,8 +15,34 @@ namespace RealEstateApp.Api.Controller
 
     public LogController(LogService logService) => _logService = logService;
 
-    [HttpGet]
-    public async Task<List<Log>> Get() => await _logService.GetAsync();
+    [Route("/ws")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public async Task Get()
+    {
+      if (HttpContext.WebSockets.IsWebSocketRequest)
+      {
+        using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+
+        while (true)
+        {
+          var message = _logService.GetLastRecords();
+          var messageSerialized = JsonSerializer.Serialize(message);
+          var bytes = Encoding.UTF8.GetBytes(messageSerialized);
+          var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+
+          if (webSocket.State == WebSocketState.Open)
+            await webSocket.SendAsync(arraySegment,
+              WebSocketMessageType.Text, true, CancellationToken.None);
+
+          else if (webSocket.State == WebSocketState.Closed ||
+            webSocket.State == WebSocketState.Aborted)
+            break;
+
+          Thread.Sleep(5000);
+        }
+      }
+      else HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+    }
 
     [HttpGet("{id:length(24)}")]
     public async Task<ActionResult<Log>> Get(string id)
