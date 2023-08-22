565aae9b3f8fea30fa4f71f99bed55eced842b79
Emir Büçkün <emirbuckun@hotmail.com>
2023-08-22 18:30:39 +0300

    Estate filter form made responsive

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -11,105 +11,84 @@ const Filter = ({ setFilter, fetchEstates }) => {
 
   return (
     <Form>
-      <Row className="mb-3">
-        <Col>
-          <Row>
-            <Col sm={5}>
-              <Form.Label>{t("estateType")}</Form.Label>
-            </Col>
-            <Col>
-              <Form.Select
-                onChange={(e) =>
-                  setFilter((prev) => ({
-                    ...prev,
-                    typeId: parseInt(e.target.value),
-                  }))
-                }
-              >
-                <option value={0}>{t("selectType")}</option>
-                {types.map((item, index) => {
-                  const { id, name } = item;
-                  return (
-                    <option key={index} value={id}>
-                      {name}
-                    </option>
-                  );
-                })}
-              </Form.Select>
-            </Col>
-          </Row>
+      <Row>
+        <Col sm={6} xs={12} className="mb-3">
+          <Form.Label>{t("estateType")}</Form.Label>
+          <Form.Select
+            onChange={(e) =>
+              setFilter((prev) => ({
+                ...prev,
+                typeId: parseInt(e.target.value),
+              }))
+            }
+          >
+            <option value={0}>{t("selectType")}</option>
+            {types.map((item, index) => {
+              const { id, name } = item;
+              return (
+                <option key={index} value={id}>
+                  {name}
+                </option>
+              );
+            })}
+          </Form.Select>
         </Col>
-        <Col>
-          <Row>
-            <Col sm={5}>
-              <Form.Label>{t("estateStatus")}</Form.Label>
-            </Col>
-            <Col>
-              <Form.Select
-                onChange={(e) =>
-                  setFilter((prev) => ({
-                    ...prev,
-                    statusId: parseInt(e.target.value),
-                  }))
-                }
-              >
-                <option value={0}>{t("selectStatus")}</option>
-                {statuses.map((item, index) => {
-                  const { id, name } = item;
-                  return (
-                    <option key={index} value={id}>
-                      {name}
-                    </option>
-                  );
-                })}
-              </Form.Select>
-            </Col>
-          </Row>
+        <Col sm={6} xs={12} className="mb-3">
+          <Form.Label>{t("estateStatus")}</Form.Label>
+          <Form.Select
+            onChange={(e) =>
+              setFilter((prev) => ({
+                ...prev,
+                statusId: parseInt(e.target.value),
+              }))
+            }
+          >
+            <option value={0}>{t("selectStatus")}</option>
+            {statuses.map((item, index) => {
+              const { id, name } = item;
+              return (
+                <option key={index} value={id}>
+                  {name}
+                </option>
+              );
+            })}
+          </Form.Select>
         </Col>
       </Row>
-      <Row className="mb-3">
-        <Col>
-          <Row>
-            <Col sm={5}>
-              <Form.Label>{t("startDate")}</Form.Label>
-            </Col>
-            <Col>
-              <Form.Control
-                name="startDate"
-                type="date"
-                onChange={(e) => {
-                  setFilter((prev) => ({
-                    ...prev,
-                    startDate: new Date(e.target.value).toISOString(),
-                  }));
-                }}
-              />
-            </Col>
-          </Row>
+      <Row>
+        <Col sm={6} xs={12} className="mb-3">
+          <Form.Label>{t("startDate")}</Form.Label>
+          <Form.Control
+            name="startDate"
+            type="date"
+            onChange={(e) => {
+              setFilter((prev) => ({
+                ...prev,
+                startDate: new Date(e.target.value).toISOString(),
+              }));
+            }}
+          />
         </Col>
-        <Col>
-          <Row>
-            <Col sm={5}>
-              <Form.Label>{t("endDate")}</Form.Label>
-            </Col>
-            <Col>
-              <Form.Control
-                name="endDate"
-                type="date"
-                onChange={(e) => {
-                  setFilter((prev) => ({
-                    ...prev,
-                    endDate: e.target.value,
-                  }));
-                }}
-              />
-            </Col>
-          </Row>
+        <Col sm={6} xs={12} className="mb-3">
+          <Form.Label>{t("endDate")}</Form.Label>
+          <Form.Control
+            name="endDate"
+            type="date"
+            onChange={(e) => {
+              setFilter((prev) => ({
+                ...prev,
+                endDate: e.target.value,
+              }));
+            }}
+          />
         </Col>
       </Row>
-      <Button onClick={fetchEstates} variant="outline-info" className="mb-3">
-        Filter
-      </Button>
+
+      <div className="d-grid gap-2">
+        <Button onClick={fetchEstates} variant="outline-info" className="mb-3">
+          Filter
+        </Button>
+      </div>
     </Form>
   );
 };
