4e57c14353961cd6935797b7c141bcbe960fb8e4
Emir Büçkün <emirbuckun@hotmail.com>
2023-08-24 10:59:16 +0300

    Estate type controller test class created

---
+++ RealEstateApp.Tests/EstateTypeControllerTests.cs
@@ -0,0 +1,15 @@
+namespace RealEstateApp.Tests
+{
+  public class EstateTypeControllerTests
+  {
+    [Fact]
+    public void AddEstateTypesAsync_VerifyDbSetAndContext()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+    }
+  }
+}
